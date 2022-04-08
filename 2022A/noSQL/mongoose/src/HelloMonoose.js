8aa8f93cd04d26b134c6c16212a7661bc40928f1
Gustavo Poli <gustavo.poli@outlook.com>
2022-04-08 11:59:39 -0300

    Mongoose - Schema v0

--- 2022A/noSQL/mongoose/src/HelloMonoose.js
+++ 2022A/noSQL/mongoose/src/HelloMonoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 async function run() {
-    mongoose.connect('mongodb://localhost:27017/poc');
+    await mongoose.connect('mongodb://localhost:27017/poc');
     
     const Customer = mongoose.model('Customer', { name: String });
 
